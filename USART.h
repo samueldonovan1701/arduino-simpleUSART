40bbb83fa2cce8714d25c9de1dd87577bac60ad2
Samuel S. Donovan <samuel.donovan1701@gmail.com>
2021-04-10 21:09:16 -0600

    Update USART.h
    Fixed functions to be const, as to work properly and not throw compiler errors.

--- USART.h
+++ USART.h
@@ -26,77 +26,77 @@ public:
         CSRC = UCSRC;
         DR = UDR;
     }
-    #define USART(N) USART(&UBRR##N##L, &UBRR##N##H, &UCSR##N##A, &UCSR##N##B, &UCSR##N##C, &UDR##N);
+    #define USART(N) USART(&UBRR##N##L, &UBRR##N##H, &UCSR##N##A, &UCSR##N##B, &UCSR##N##C, &UDR##N)
 
 //UCSRA//
     //RxC (R)
-    bool RxComplete()
+    bool RxComplete() const
     {
         return *CSRA & 0b10000000; // != 0
     }
 
     //TxC (W/R)
-    bool TxComplete()
+    bool TxComplete() const
     {
         return *CSRA & 0b01000000; // != 0
     }
-    void SetTxComplete()
+    void SetTxComplete() const
     {
         *CSRA |= 0b01000000;
     }
-    void ClearTxComplete()
+    void ClearTxComplete() const
     {
         *CSRA &= 0b10111111;
     }
 
     //UDRE (R)
-    bool DataRegisterEmpty()
+    bool DataRegisterEmpty() const
     {
         return *CSRA & 0b00100000; //!= 0
     }
 
     //FE (R)
-    bool FrameError()
+    bool FrameError() const
     {
         return *CSRA & 0b00010000; //!= 0
     }
 
     //DOR (R)
-    bool DataOverrunError()
+    bool DataOverrunError() const
     {
         return *CSRA & 0b00001000; //!= 0
     }
 
     //PE (R)
-    bool ParityError()
+    bool ParityError() const
     {
         return *CSRA & 0b00000100; //!= 0
     }
 
     //U2X (R/W)
-    bool DoubleTransmissionSpeedIsEnabled()
+    bool DoubleTransmissionSpeedIsEnabled() const
     {
         return *CSRA & 0b00000010; //!= 0
     }
-    void EnableDoubleTransmissionSpeed()
+    void EnableDoubleTransmissionSpeed() const
     {
         *CSRA |= 0b00000010;
     } 
-    void DisableDoubleTransmissionSpeed()
+    void DisableDoubleTransmissionSpeed() const
     {   
         *CSRA &= 0b11111101;
     }
 
     //MPCM (R/W)
-    bool MultiProcessorCommunicationModeIsEnabled()
+    bool MultiProcessorCommunicationModeIsEnabled() const
     {
         return *CSRA & 0b00000001;
     }
-    void EnableMultiProcessorCommunicationMode()
+    void EnableMultiProcessorCommunicationMode() const
     {
         *CSRA |= 0b00000001;
     }
-    void DisableMultiProcessorCommunicationMode()
+    void DisableMultiProcessorCommunicationMode() const
     {
         *CSRA &= 0b11111110;
     }
@@ -106,90 +106,93 @@ public:
 
 //UCSRB//
     //RXCIE (R/W)
-    bool RxCompleteInterruptIsEnabled()
+    bool RxCompleteInterruptIsEnabled() const
     {
         return *CSRB & 0b10000000; //!= 0
     }
-    void EnableRxCompleteInterrupt()
+    void EnableRxCompleteInterrupt() const
     {
        *CSRB |= 0b10000000;
+       sei();
     }
-    void DisableRxCompleteInterrupt()
+    void DisableRxCompleteInterrupt() const
     {
         *CSRB &= 0b01111111;
     }
 
     //TXCIE (R/W)
-    bool TxCompleteInterruptIsEnabled()
+    bool TxCompleteInterruptIsEnabled() const
     {
         return *CSRB & 0b01000000; //!= 0
     }
-    void EnableTxCompleteInterrupt()
+    void EnableTxCompleteInterrupt() const
     {
         *CSRB |= 0b01000000;
+        sei();
     }
-    void DisableTxCompleteInterrupt()
+    void DisableTxCompleteInterrupt() const
     {
         *CSRB &= 0b10111111;
     }
 
     //UDRIE (R/W)
-    bool DataRegisterEmptyInterruptIsEnabled()
+    bool DataRegisterEmptyInterruptIsEnabled() const
     {
         return *CSRB & 0b00100000; //!= 0
     }
-    void EnableDataRegisterEmptyInterrupt()
+    void EnableDataRegisterEmptyInterrupt() const
     {
         *CSRB |= 0b00100000;
+        sei();
     }
-    void DisableDataRegisterEmptyInterrupt()
+    void DisableDataRegisterEmptyInterrupt() const
     {
         *CSRB &= 0b11011111;
     }
 
     //RXEN (R/W)
-    bool RxIsEnabled()
+    bool RxIsEnabled() const
     {
         return *CSRB & 0b00010000; //!= 0
     }
-    void EnableRx()
+    void EnableRx() const
     {
         *CSRB |= 0b00010000;    
     }
-    void DisableRx()
+    void DisableRx() const
     {
         *CSRB &= 0b11101111;
     }
 
     //TXEN (R/W)
-    bool TxIsEnabled()
+    bool TxIsEnabled() const
     {
         return *CSRB & 0b00001000; //!= 0
     }
-    void EnableTx()
+    void EnableTx() const
     {
         *CSRB |= 0b00001000;
     }
-    void DisableTx()
+    void DisableTx() const
     {
         *CSRB &= 0b11110111;
     }
 
     //UCSZ2 (R/W) (See FrameSize funcs)
     //RXB8 (R)
-    byte GetRxBit9()
+    byte GetRxBit9() const
     {
         return (*CSRB & 0b00000010) >> 1;
     }
-    byte GetTxBit9()
+    byte GetTxBit9() const
     {
         return *CSRB & 0b00000001;
     }
-    void SetTxBit9()
+    void SetTxBit9() const
     {
         *CSRB |= 0b00000001;
     }
-    void ClearTxBit9()
+    void ClearTxBit9() const
     {
         *CSRB &= 0b11111110;
     }
@@ -201,11 +204,11 @@ public:
        SYNC = 0b01,
        MSPIM = 0b11  
     };
-    Mode GetMode()
+    Mode GetMode() const
     {
         return (Mode)((*CSRC & 0b11000000) >> 6);
     }
-    void SetMode(Mode mode)
+    void SetMode(Mode mode) const
     {
         *CSRC &= 0b00111111; //Clear
         *CSRC |= mode << 6; //Set
@@ -218,11 +221,11 @@ public:
         EVEN = 0b10,
         ODD = 0b11
     };
-    Parity GetParity()
+    Parity GetParity() const
     {
         return (Parity)((*CSRC & 0b00110000) >> 4);
     }
-    void SetParity(Parity mode)
+    void SetParity(Parity mode) const
     {
         *CSRC &= 0b11001111; //Clear
         *CSRC |= mode << 4; //Set
@@ -233,11 +236,11 @@ public:
         ONE = 0,
         TWO = 1
     };
-    StopBits GetStopBits()
+    StopBits GetStopBits() const
     {
         return (StopBits)((*CSRC & 0b00001000) >> 3);
     }
-    void SetStopBits(StopBits size)
+    void SetStopBits(StopBits size) const
     {
         *CSRC &= 0b11110111; //Clear
         *CSRC |= size << 3; //Set
@@ -251,11 +254,11 @@ public:
         EIGHT = 0b011,
         NINE = 0b111
     };
-    FrameSize GetFrameSize()
+    FrameSize GetFrameSize() const
     {
         return (FrameSize)((*CSRB & 0b00000100) + ((*CSRC & 0b00000110) >> 1));
     }
-    void SetFrameSize(FrameSize size) //size can be 5-9 bits
+    void SetFrameSize(FrameSize size) const//size can be 5-9 bits
     {
         *CSRC &= 0b11111001; //Clear UCSZ 0 & 1
         *CSRB &= 0b11111011; //Clear UCSZ 2
@@ -269,11 +272,11 @@ public:
         RISING_EDGE = 0,
         FALLING_EDGE = 1
     };
-    ClockPolarity GetClockPolarity()
+    ClockPolarity GetClockPolarity() const
     {
         return (ClockPolarity)(*CSRC & 0b00000001);
     }
-    void SetClockPolarity(ClockPolarity polarity)
+    void SetClockPolarity(ClockPolarity polarity) const
     {
         *CSRC &= 0b11111110; //Clear
         *CSRC |= polarity; //Set
@@ -281,7 +284,7 @@ public:
 
 
 //Core Funcs
-    void SetBaudRate(unsigned long baud_rate)
+    void SetBaudRate(unsigned long baud_rate) const
     {
         unsigned short val = 0;
         
@@ -295,41 +298,41 @@ public:
         *BRRH = (val >> 8) & 0b00001111; //Set URBBH
         *BRRL = val; //Set UBRRL
     }
-    void Write(char data)
+    void Write(char data) const
     {
         while(!DataRegisterEmpty()){}
         *DR = data;
     }
-    void Write(char* buffer)
+    void Write(const char* buffer) const
     {
         for(byte i = 0; buffer[i] != 0; i++)
         {
             Write(buffer[i]);
         } 
     }
-    void Write(int i, int base=10)
+    void Write(int i, int base=10) const
     {
         char buf[20];
         itoa(i, buf, base);
         Write(buf);
     }
-    void Writeln(char* buffer)
+    void Writeln(const char* buffer) const
     {
         Write(buffer);
         Write('\n');
         Write('\r');
     }
-    void Writeln(int i, int base=10)
+    void Writeln(int i, int base=10) const
     {
         Write(i, base);
         Write('\n');
         Write('\r');
     }
-    byte Read()
+    byte Read() const
     {
         return *DR;
     }
-    void begin(unsigned long baud_rate)
+    void begin(unsigned long baud_rate) const
     {
         SetFrameSize(EIGHT);
         SetStopBits(ONE);
